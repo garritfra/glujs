67f2fe06d24cf550a84403c8c2d15f42642b733a
garritfra <garritfranke@gmail.com>
2018-03-27 00:18:23 +0200

    refactor math functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-export function add(a, b) {
-  return a + b;
-}
-
-export function subtract(a, b) {
-  return a - b;
-}
-
-export function multiply(a, b) {
-  return a * b;
-}
-
-export function divide(a, b) {
-  return a / b;
-}
+export { add, subtract, multiply, divide } from './math'
