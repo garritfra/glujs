d4c269231c520a96dd93fcf3395a6a5258a530ca
garritfra <garritfranke@gmail.com>
2018-03-26 23:05:11 +0200

    comment test

--- src/test/index.js
+++ src/test/index.js
@@ -3,8 +3,8 @@
 import { add, subtract, multiply, divide } from "../lib/math";
 import { assert } from "mocha";
 
-describe('mocha and jsverify', function () {
+/* describe('mocha and jsverify', function () {
   property('are working', function () {
     return true
   })
-});
+}); */
