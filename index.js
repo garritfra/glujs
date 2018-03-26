65ea4743045c03a38afb091f38de813f4de72cec
garritfra <garritfranke@gmail.com>
2018-03-26 21:23:42 +0200

    add math functions

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-exports.test = function() {
-  return "test";
-}
+require("babel-core").transform("code", {
+  plugins: ["transform-es2015-modules-commonjs"]
+});
+
+export {add, subtract, multiply, divide} from './modules/math';
