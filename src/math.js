23e65a2a93be6f7aa3e5cb80bb3d84bec50aa2ee
garritfra <garritfranke@gmail.com>
2018-03-27 19:21:59 +0200

    add jsdoc

--- src/math.js
+++ src/math.js
@@ -1,19 +1,43 @@
+
+/**
+ * Adds two numbers
+ * @param {number} a number 1
+ * @param {number} b number 2
+ */
 export function add(a, b) {
   return a + b;
 }
 
+/**
+ * Subtracts two numbers
+ * @param {number} a number 1
+ * @param {number} b number 2
+ */
 export function subtract(a, b) {
   return a - b;
 }
 
+/**
+ * Multiplies two numbers
+ * @param {number} a number 1
+ * @param {number} b number 2
+ */
 export function multiply(a, b) {
   return a * b;
 }
 
+/**
+ * Divides two numbers
+ * @param {number} a number 1
+ * @param {number} b number 2
+ */
 export function divide(a, b) {
   return a / b;
 }
-
+/**
+ * returns the sum of an array
+ * @param {Array<number>} arr Array
+ */
 export function sum(arr) {
   if (typeof arr === "number") {
     return arr;
