b8172018ff246cbb48e4e4c65451a2f23f6f4f5d
garritfra <garritfranke@gmail.com>
2018-03-28 18:42:20 +0200

    add constants

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-export { add, subtract, multiply, divide } from './math'
+//Constants
+export {
+  PI,
+  HALF_PI,
+  QUARTER_PI,
+  e,
+  GRAVITY_EARTH,
+  SPEED_OF_LIGHT,
+  STANDARD_ATMOSPHERE
+} from "./constants";
+
+//Functions related to Math
+export { add, subtract, multiply, divide } from "./math";
+
+//Utility Functions
+export { now } from "./utils";
