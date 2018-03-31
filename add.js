7c59e0d86e09c45c3aade291f51dc5af4dabf6c1
garritfra <garritfranke@gmail.com>
2018-03-31 10:16:15 +0200

    compile add.js

---
+++ add.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+/**
+ * Adds two numbers
+ * @param {number} a number 1
+ * @param {number} b number 2
+ */
+function add(a, b) {
+    return a + b;
+}
+exports["default"] = add;
