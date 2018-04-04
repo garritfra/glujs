7534047510894354cd859097c951827a588be2d0
garritfra <garritfranke@gmail.com>
2018-04-04 17:20:11 +0200

    fix lint

--- glu.js
+++ glu.js
@@ -93,20 +93,20 @@ exports.now = function() {
 
 /**
  * Capitalizes the first letter of a string
- * @param {string} string 
+ * @param {string} string
  */
 function capitalizeString(str) {
-    var lowercaseStr = str.toLowerCase();
-    var firstLetter = lowercaseStr[0].toUpperCase();
-    var remainingWord = lowercaseStr.substr(1);
-    var capitalizedString = firstLetter + remainingWord;
-    return capitalizedString;
+  var lowercaseStr = str.toLowerCase();
+  var firstLetter = lowercaseStr[0].toUpperCase();
+  var remainingWord = lowercaseStr.substr(1);
+  var capitalizedString = firstLetter + remainingWord;
+  return capitalizedString;
 }
 exports.capitalizeString = capitalizeString;
 
 /**
  * Converts a string to upper case
- * @param {string} str 
+ * @param {string} str
  */
 function toUpper(str) {
   return str.toUpperCase();
@@ -115,7 +115,7 @@ exports.toUpper = toUpper;
 
 /**
  * Converts a string to lower case
- * @param {string} str 
+ * @param {string} str
  */
 function toLower(str) {
   return str.toLowerCase();
