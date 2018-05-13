f48faf2ea2ba70d156b28ee4c13bae616f5722de
garritfra <garritfranke@gmail.com>
2018-05-13 22:26:35 +0200

    Use Object.assign syntax

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ const constants = require("./components/constants");
 const math = require("./components/math");
 const strings = require("./components/strings");
 
-module.exports = {
-  ...constants,
-  ...math,
-  ...strings
-};
+module.exports = Object.assign({}, constants, math, strings);
