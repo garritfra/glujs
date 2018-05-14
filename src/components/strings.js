d4192ac5375c798e5a5f02344d275eb0a984ca59
garritfra <garritfranke@gmail.com>
2018-05-14 10:47:21 +0200

    use const instead of var

--- src/components/strings.js
+++ src/components/strings.js
@@ -1,18 +1,19 @@
 /**
  * Capitalizes the first letter of a string
  * @param {string} string
+ * @returns {string} capitalized string
  */
 exports.capitalizeString = str => {
-  var lowercaseStr = str.toLowerCase();
-  var firstLetter = lowercaseStr[0].toUpperCase();
-  var remainingWord = lowercaseStr.substr(1);
-  var capitalizedString = firstLetter + remainingWord;
-  return capitalizedString;
+  const lowercaseStr = str.toLowerCase();
+  const firstLetter = lowercaseStr[0].toUpperCase();
+  const remainingWord = lowercaseStr.substr(1);
+  return firstLetter + remainingWord;
 };
 
 /**
  * Converts a string to upper case
- * @param {string} str
+ * @param {string} string
+ * @returns {string} uppercase string
  */
 exports.toUpper = str => {
   return str.toUpperCase();
@@ -20,7 +21,8 @@ exports.toUpper = str => {
 
 /**
  * Converts a string to lower case
- * @param {string} str
+ * @param {string} string
+ * @returns {string} lowercase string
  */
 exports.toLower = str => {
   return str.toLowerCase();
