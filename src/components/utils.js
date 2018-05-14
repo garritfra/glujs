c678326c90cd05ca478a09f4970ac516f871e6c0
garritfra <garritfranke@gmail.com>
2018-05-14 10:54:39 +0200

    add random functions skeleton

--- src/components/utils.js
+++ src/components/utils.js
@@ -5,3 +5,17 @@
 exports.now = () => {
   return new Date();
 };
+
+/**
+ * generates a random number between 0 and 1
+ * @returns {number} random number between 0 and 1
+ */
+exports.random = () => {};
+
+/**
+ * generates a random number between the two specified parameters
+ * @param {number} from
+ * @param {number} to
+ * @returns {number} random number in between the two specified numbers
+ */
+exports.randomInt = (from, to) => {};
