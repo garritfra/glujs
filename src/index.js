2cc5026bf4a9aa5a5a7b65811e470d8e0fd6620b
garritfra <garritfranke@gmail.com>
2018-05-14 10:57:22 +0200

    unclude utils in index file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const constants = require("./components/constants");
 const math = require("./components/math");
 const strings = require("./components/strings");
+const utils = require("./components/utils");
 
-module.exports = Object.assign({}, constants, math, strings);
+module.exports = Object.assign({}, constants, math, strings, utils);
