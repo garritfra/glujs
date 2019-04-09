8c65b9d607ff4864cab9c310bb74ebd81b11b0f5
garritfra <garritfranke@gmail.com>
2019-04-09 18:20:55 +0200

    Fix require statements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-module.exports = Object.assign(
-  {},
-  require("./components/constants"),
-  require("./components/math"),
-  require("./components/strings"),
-  require("./components/utils")
-);
+module.exports = {
+  ...require("./components/constants"),
+  ...require("./components/math"),
+  ...require("./components/strings"),
+  ...require("./components/utils"),
+  ...require("./components/arrays")
+};
